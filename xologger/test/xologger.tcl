625b10931970fdcb4381f51b01f070812119299d
Ben Thomasson <ben.thomasson@gmail.com>
2009-01-07 09:18:53 -0500

    removing xologger as it is empty

--- xologger/test/xologger.tcl
+++
@@ -1,2 +0,0 @@
-# Created at Wed Nov 28 09:08:45 AM EST 2007 by bthomass
-
--- xologger/xologger.tcl
+++
@@ -1,8 +0,0 @@
-# Created at Wed Nov 28 09:08:45 AM EST 2007 by bthomass
-
-package require XOTcl
-package require xox
-
-::xox::Package create ::xologger
-::xologger loadAll
-
