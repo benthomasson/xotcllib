da5a61441a81087ff84ddec0fea243d4a83b5321
Ben Thomasson <ben.thomasson@gmail.com>
2009-01-07 09:51:17 -0500

    removing xointerp since obsolete

--- xointerp/test/pkgIndex.tcl
+++
@@ -1,2 +0,0 @@
-package ifneeded xointerp 1.0 [list source [file join $dir xointerp.tcl ]]
-package ifneeded xointerp::test 1.0 [list source [file join $dir test xointerp.tcl ]]
--- xointerp/test/xointerp.tcl
+++
@@ -1,2 +0,0 @@
-# Created at Wed Jan 16 21:12:09 EST 2008 by bthomass
-
