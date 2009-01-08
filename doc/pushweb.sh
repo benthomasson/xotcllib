ef3a88439a3c49ef10225714959d58db7d77af7f
Ben Thomasson <ben.thomasson@gmail.com>
2009-01-07 21:42:41 -0500

    changing pushweb.sh to use nearlyfreespeech.net

--- doc/pushweb.sh
+++ doc/pushweb.sh
@@ -1,16 +1,1 @@
-#!/usr/bin/tclsh
-set to bthomass@xotcllib.gforge.cisco.com:/home/groups/xotcllib/htdocs/
-foreach file [ glob -nocomplain *.html ] {
-    puts "Pushing $file to $to"
-    catch { exec scp $file $to >&@ stdout }
-}
-foreach dir [ glob -nocomplain * ] {
-
-    if { ! [ file isdirectory $dir ] } { continue }
-    if [ string match _* $dir ] { continue }
-    file mkdir [ file join $to $dir ]
-    foreach file [ glob -nocomplain [ file join $dir *.html ] ] {
-        puts "Pushing $file to [ file join $to $dir ]"
-        catch { exec scp $file [ file join $to $dir ] >&@ stdout }
-    }
-}
+scp -r docs/* pyrolyte_xotcllib@ssh.phx.nearlyfreespeech.net:/home/public/
